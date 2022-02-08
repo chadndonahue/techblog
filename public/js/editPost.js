82cdd1c32b3671f4e3d06b370f76c31aa862186f
Chad <chadndonahue@gmail.com>
2022-02-07 20:20:02 -0500

    finished and deploying to heroku

---
+++ public/js/editPost.js
@@ -0,0 +1,22 @@
+const editPostHandler = async(event) => {
+    event.preventDefault();
+    const name = document.querySelector('#name').value.trim();
+    const description = document.querySelector('#description').value.trim();
+    const id = window.location.toString().split('/')[4]
+    if (name && description) {
+        const response = await fetch('/api/posts/' + id, {
+            method: 'PUT',
+            body: JSON.stringify({ name, description }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        if(response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText)
+        }
+    }
+};
+
+document
+.querySelector("#editBtn")
+.addEventListener('click', editPostHandler);
